12c8eda1b6a70e8e52880766c675b2aab67cdc9f
naser <snfree89@gmail.com>
2022-04-11 19:04:43 +0300

    upgrade app

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:reactive_todo_app/ui/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Reactive Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        canvasColor: Colors.transparent
+      ),
+      //Our only screen/page we have
+      home: HomePage(title: 'My Todo List'),
+    );
+  }
+}
+
