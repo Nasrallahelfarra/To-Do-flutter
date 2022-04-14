4a3c7dfcd9ae5c5b2e282d74db9ed2c436a65c73
naser <snfree89@gmail.com>
2022-04-14 14:27:14 +0300

    fix test automation

--- test_driver/app_start.dart
+++ test_driver/app_start.dart
@@ -1,9 +1,24 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
+import 'package:objectbox/objectbox.dart';
+import 'package:reactive_todo_app/core/object_box/object_box.dart';
+import 'package:reactive_todo_app/features/todo/data/data_source/todo_local_data_source.dart';
+import 'package:reactive_todo_app/features/todo/data/model/todo.dart';
+import 'package:reactive_todo_app/features/todo/data/repository/todo_repository.dart';
+import 'package:reactive_todo_app/features/todo/presentation/cubit/todo_cubit.dart';
 import 'package:reactive_todo_app/main.dart';
 main() async {
   enableFlutterDriverExtension();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  objectBox = await ObjectBox.create();
+  final store = objectBox.store;
+
+  sl.registerFactory<Box<Todo>>(() => store.box<Todo>());
+  sl.registerFactory<TodoLocalDataSource>(() => TodoLocalDataSource(sl()));
+  sl.registerFactory<TodoRepository>(() => TodoRepository(sl()));
+  sl.registerFactory<TodoCubit>(() => TodoCubit(sl()));
   runApp(MyApp());
 
 }
