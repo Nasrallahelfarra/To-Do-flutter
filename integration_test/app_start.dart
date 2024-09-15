d7b996115592c129336fb93f248637622b4b0da0
Nasrallahelfarra <51306301+Nasrallahelfarra@users.noreply.github.com>
2024-09-15 15:03:35 +0300

    update integration test

--- integration_test/app_start.dart
+++ integration_test/app_start.dart
@@ -1,36 +1,28 @@
+
 import 'package:flutter/material.dart';
-import 'package:flutter_driver/driver_extension.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:objectbox/objectbox.dart';
 import 'package:reactive_todo_app/core/object_box/object_box.dart';
-import 'package:reactive_todo_app/features/todo/data/data_source/todo_local_data_source.dart';
-import 'package:reactive_todo_app/features/todo/data/model/todo.dart';
-import 'package:reactive_todo_app/features/todo/data/repository/todo_repository.dart';
-import 'package:reactive_todo_app/features/todo/presentation/cubit/todo_cubit.dart';
 import 'package:reactive_todo_app/main.dart';
 
 import 'steps/add_to_do_step.dart';
 import 'steps/edite_to_do_step.dart';
 import 'steps/remove_to_do_step.dart';
 import 'steps/view_home_steps.dart';
+import 'package:integration_test/integration_test.dart';
 
 main() async {
-  enableFlutterDriverExtension();
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
+  final objectBox = await ObjectBox.create();
+  await registerFactories(store: objectBox.store);
 
-  final ObjectBox objectBox = await ObjectBox.create();
-  final store = objectBox.store;
-
-  sl.registerFactory<Box<Todo>>(() => store.box<Todo>());
-  sl.registerFactory<TodoLocalDataSource>(() => TodoLocalDataSource(sl()));
-  sl.registerFactory<TodoRepository>(() => TodoRepository(sl()));
-  sl.registerFactory<TodoCubit>(() => TodoCubit(sl()));
   group('TODO :', () {
     testWidgets(
       'App TODO ',
       (WidgetTester tester) async {
         // Initialing Application for Testing and waiting for it to launch
-        tester.pumpWidget(MyApp());
+        await tester.pumpWidget(MyApp());
+        await tester.pumpAndSettle(Duration(seconds: 3));
         ViewHomeSteps viewHomeSteps=ViewHomeSteps(tester);
         AddToDo addToDo=AddToDo(tester);
         EditToDo editToDo=EditToDo(tester);
@@ -40,8 +32,8 @@ main() async {
         await editToDo.main();
         await removeToDo.main();
       },
-      skip: false,
-      timeout: const Timeout(Duration(minutes: 5)),
+     /* skip: false,
+      timeout: const Timeout(Duration(minutes: 5)),*/
     );
   });
 }
--- integration_test/utilities.dart
+++ integration_test/utilities.dart
@@ -9,12 +9,11 @@ class Utilities {
   static late TestRun newRun;
   static int passed = 1;
   static int failed = 5;
-  static Future<void> setupAndGetDriver() async {
-
-  }
+  static Future<void> setupAndGetDriver() async {}
   static Future<void> closeDriver() async {
     sleep(const Duration(seconds: 5));
   }
+
   static Future<void> testRailInitialize() async {
     TestRail.configure(
         username: 'xxxxxxx@gmail.com',
@@ -23,9 +22,11 @@ class Utilities {
     //Create test run
     await createTestRun();
   }
-  static  Future<void> createTestRun()async {
+
+  static Future<void> createTestRun() async {
     newRun = await TestRun.create(
-        name: '${DateTime.now().toIso8601String()} Test Run To Do', projectId:3);
+        name: '${DateTime.now().toIso8601String()} Test Run To Do',
+        projectId: 3);
     //Add cases to the run
     await newRun.update(
       caseIds: [
@@ -46,27 +47,25 @@ class Utilities {
       ],
     );
   }
-  static Future<void> requestData(String element) async {
-  }
-  static Future <void>setTestStatus(int idTestCase, int statusId) async {
-   /* await newRun.addResultForCase(
+
+  static Future<void> requestData(String element) async {}
+  static Future<void> setTestStatus(int idTestCase, int statusId) async {
+    /* await newRun.addResultForCase(
       idTestCase,
       statusId: statusId,
     );*/
   }
-  static Future<void> tap(Finder element,WidgetTester tester,{required int testCaseId}) async {
+  static Future<void> tap(Finder element, WidgetTester tester,
+      {required int testCaseId}) async {
     try {
       await tester.tap(element);
-      await tester.pumpAndSettle();
-      await tester.pump(Duration(seconds: 2));
+      await tester.pumpAndSettle(Duration(seconds: 2));
+
       await setTestStatus(testCaseId, passed);
-
-    } on TimeoutException catch(e) {
+    } on TimeoutException catch (e) {
       await setTestStatus(testCaseId, failed);
-
     } catch (e) {
       await setTestStatus(testCaseId, failed);
-
     }
   }
   /*static Future<void> verifyElementText(Finder element, String text,{required int testCaseId}) async {
@@ -87,88 +86,77 @@ class Utilities {
     String actual = await driver!.getText(element);
     return actual;
   }*/
-  static Future<void> waitForElement(Finder element,WidgetTester tester,{required int testCaseId}) async {
+  static Future<void> waitForElement(Finder element, WidgetTester tester,
+      {required int testCaseId}) async {
     try {
-      await tester.pump(Duration(seconds: 5));
-      await tester.pumpAndSettle();
+      await tester.pump(Duration(seconds: 2));
       expect(element, findsOneWidget);
+      await tester.pumpAndSettle();
 
       // Verify the counter increments by 1.
       await setTestStatus(testCaseId, passed);
-
-    } on TimeoutException catch(e) {
+    } on TimeoutException catch (e) {
       print('Test Fail');
       await setTestStatus(testCaseId, failed);
     } catch (e) {
       await setTestStatus(testCaseId, failed);
-
     }
   }
 
-
-  static Future<void> fillTextThenWait(Finder element, String text,WidgetTester tester,
-      {required int testCaseId}  ) async {
+  static Future<void> fillTextThenWait(
+      Finder element, String text, WidgetTester tester,
+      {required int testCaseId}) async {
     try {
-      await  tester.enterText(element, text);
+      await tester.enterText(element, text);
       expect(find.text(text), findsOneWidget);
       await setTestStatus(testCaseId, passed);
-
-    } on TimeoutException catch(e) {
+    } on TimeoutException catch (e) {
       await setTestStatus(testCaseId, failed);
-
     } catch (e) {
       await setTestStatus(testCaseId, failed);
-
     }
   }
 
-
-  static Future<void> waitForElementAbsent(Finder element,WidgetTester tester,{required int testCaseId}) async {
+  static Future<void> waitForElementAbsent(Finder element, WidgetTester tester,
+      {required int testCaseId}) async {
     try {
       await tester.pump(Duration(seconds: 2));
-      await tester.pumpAndSettle();
       expect(element, findsNothing);
       await setTestStatus(testCaseId, passed);
-    } on TimeoutException catch(e) {
+    } on TimeoutException catch (e) {
       await setTestStatus(testCaseId, failed);
-
     } catch (e) {
       await setTestStatus(testCaseId, failed);
-
     }
   }
 
-  static Future<void> isPresented(Finder element,WidgetTester tester,{required int testCaseId}
-      ) async {
+  static Future<void> isPresented(Finder element, WidgetTester tester,
+      {required int testCaseId}) async {
     try {
-      expect(await Utilities.isPresent(element,tester,testCaseId:testCaseId ), true);
+      expect(await Utilities.isPresent(element, tester, testCaseId: testCaseId),
+          true);
       await setTestStatus(testCaseId, passed);
-
-    } on TimeoutException catch(e) {
+    } on TimeoutException catch (e) {
       await setTestStatus(testCaseId, failed);
-
     } catch (e) {
       await setTestStatus(testCaseId, failed);
-
     }
-
   }
-  static Future<bool> isPresent(Finder finder,WidgetTester tester,
-      {Duration timeout = const Duration(seconds: 5),required int testCaseId}) async {
+
+  static Future<bool> isPresent(Finder finder, WidgetTester tester,
+      {Duration timeout = const Duration(seconds: 1),
+      required int testCaseId}) async {
     Stopwatch s = Stopwatch();
     s.start();
     try {
       await tester.pump(timeout);
-      expect(finder, findsOneWidget);
+      expect(finder, findsOneWidget);updae
       await tester.pumpAndSettle();
       await setTestStatus(testCaseId, passed);
-
-    } on TimeoutException catch(e) {
+    } on TimeoutException catch (e) {
       await setTestStatus(testCaseId, failed);
-
     } catch (e) {
       await setTestStatus(testCaseId, failed);
-
     }
     s.stop();
     if (s.elapsedMilliseconds >= timeout.inMilliseconds) {
@@ -177,5 +165,4 @@ class Utilities {
       return true;
     }
   }
-
 }
